691895673111cd2840f7d0051859c02e401a0dcd
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-28 00:17:40 +0300

    move behind make the next pointer to the next node after the current one

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,38 +1,53 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
- * delete_dnodeint_at_index - deletes a node at a given position
- * @head: pointer to the first node
- * @index: node position to remove
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = *head;
-	size_t iter = 0;
-	int flag = -1;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-	if (!head || !*head)
+	if (*head == NULL)
 	{
-		return (flag);
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
 	}
 	else
 	{
-		while (current && index != iter)
-		{
-			current = current->next;
-			iter++;
-		}
-		if (!current)
-			return (flag);
-		if (current->next)
-			current->next->prev = current->prev;
-		if (index == 0)
-			(*head) = current->next;
-		else
-			current->prev->next = current->next;
-		free(current);
-		return (flag * -1);
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
 	}
-	return (flag);
+	return (1);
 }
